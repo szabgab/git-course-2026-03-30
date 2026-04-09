e09780dc646739a819a2fd09ee43ef0484f7d1d9
Gabor Szabo <gabor@szabgab.com>
2026-04-09 11:05:43 +0200

    make fibo_modulo function

--- fibo_modulo.py
+++ fibo_modulo.py
@@ -1,13 +1,16 @@
 import sys
 from myfibo import fibo
 
-if len(sys.argv) != 2:
-    exit(f"Usage: {sys.argv[0]} DIV")
+def fibo_modulo(n):
+    for n in range(100):
+        fib = fibo(n)
+        if fib % div == 0:
+            print(f"fibo({n}) == {fib} can be divided by {div}")
 
-div = int(sys.argv[1])
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        exit(f"Usage: {sys.argv[0]} DIV")
 
-for n in range(100):
-    fib = fibo(n)
-    if fib % div == 0:
-        print(f"fibo({n}) == {fib} can be divided by {div}")
+    div = int(sys.argv[1])
+    fibo_modulo(div)
 
